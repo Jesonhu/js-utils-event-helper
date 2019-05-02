9755da3b7b4e3ad84d2b334b14a2edcf5abaad36
Jesonhu <jesonhu_web@163.com>
2019-05-02 09:41:00 +0800

    chore: 添加注释.

--- src/Core/events/interface/IEventBin.ts
+++ src/Core/events/interface/IEventBin.ts
@@ -8,26 +8,32 @@ import { IEventDispatcher } from './IEventDispatcher';
 export interface IEventBin {
   type: string;
   /**
+   * 事件处理函数.
    * @private
    */
   listener: Function;
   /**
+   * 事件处理函数中的this指向.
    * @private
    */
   thisObject: any;
   /**
+   * 优先级.
    * @private
    */
   priority: number;
   /**
+   * 事件处理函数中的 this
    * @private
    */
   target: IEventDispatcher;
   /**
+   * 是否可以捕获.
    * @private
    */
   useCapture: boolean;
   /**
+   * 是否只派发一次, 即 `once` 方式监听的事件处理函数.
    * @private
    */
   dispatchOnce: boolean;
