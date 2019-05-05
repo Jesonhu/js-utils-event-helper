fdcf56b682552466240a83056f368d91c0857c12
Jesonhu <jesonhu_web@163.com>
2019-05-06 07:45:18 +0800

    chore: 编译.

---
+++ dist/types/Core/EventTest.d.ts
@@ -0,0 +1,4 @@
+import { Event } from './events/Event';
+export declare class EventTest extends Event {
+    static test: string;
+}
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,9 +1,12 @@
 import { Event } from './Core/events/Event';
 import { EventDispatcher } from './Core/events/EventDispatcher';
+import { EventTest } from './Core/EventTest';
 /**
  * 常用工具合集.
  */
 declare const jsUtilsHelper: {
+    /** 事件类型类 */
+    EventTest: typeof EventTest;
     /** 事件类 */
     Event: typeof Event;
     /**
