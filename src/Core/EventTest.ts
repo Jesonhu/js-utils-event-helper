c6da4ba2fadeae3097a479981ff354251af0308c
Jesonhu <jesonhu_web@163.com>
2019-05-06 07:43:26 +0800

    feat: 添加测试事件类型类.

---
+++ src/Core/EventTest.ts
@@ -0,0 +1,5 @@
+import { Event } from './events/Event';
+
+export class EventTest extends Event {
+  static test: string = 'test';
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import Core from './Core/index';
 import { Event } from './Core/events/Event';
 import { EventDispatcher } from './Core/events/EventDispatcher';
+import { EventTest } from './Core/EventTest';
 /**
  * 常用工具合集.
  */
 const  jsUtilsHelper = {
+  /** 事件类型类 */
+  EventTest,
   /** 事件类 */
   Event,
   /**
