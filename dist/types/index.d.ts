6ef9681612413e14aba3c25e5437dedbe3624074
Jesonhu <jesonhu_web@163.com>
2019-05-05 22:26:26 +0800

    chore: 导出 event 类.

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,8 +1,11 @@
+import { Event } from './Core/events/Event';
 import { EventDispatcher } from './Core/events/EventDispatcher';
 /**
  * 常用工具合集.
  */
 declare const jsUtilsHelper: {
+    /** 事件类 */
+    Event: typeof Event;
     /**
      * 事件监听派发类.
      */
